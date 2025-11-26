018eab3d0f046bc21180a72b6c2af948c1de3f47
csharjeel360-eng <csharjeel360@gmail.com>
2025-11-26 12:12:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ const corsOptions = {
     
     const allowedOrigins = [
       process.env.CLIENT_URL,
-      'http://localhost:3000',
       'http://localhost:5173',
       'https://yourdomain.com' // Add your production domain
     ];
